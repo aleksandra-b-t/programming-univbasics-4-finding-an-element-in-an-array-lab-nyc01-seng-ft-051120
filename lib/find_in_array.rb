3fd75e73fd4655c4fccbfe269596fcc77b3c1e2f
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-27 17:27:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,11 @@
 def find_element_index(array, value_to_find)
   counter = 0
+  found_valuse_index = nil
   while counter < array.length do
-    puts array[counter] == value_to_find
+    if array[counter] == value_to_find
+      found_valuse_index = counter
+    end
     counter += 1;
   end
+  found_valuse_index
 end
